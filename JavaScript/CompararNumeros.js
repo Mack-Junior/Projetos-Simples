0fc44cdd79d2c67575943454f4a85ac96e6aab8a
Mack Junior <130861538+Mack-Junior@users.noreply.github.com>
2026-02-24 13:10:04 -0300

    Estou subindo aqui um dos meus primeiros projetos em js
    um script pra comparar números se 2 números do input do usuário são idênticos

---
+++ JavaScript/CompararNumeros.js
@@ -0,0 +1,8 @@
+var numeroUm = Number(prompt("Digite um número: "));
+var numeroDois = Number(prompt("Digite um número pra testar igualdade com o primeiro: "));
+
+if (numeroUm === numeroDois) {
+    console.log("Números iguais")
+} else {
+    console.log("Números diferentes")
+}
